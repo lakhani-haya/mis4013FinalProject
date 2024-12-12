bf8a195da1a4c7fa9a5ab523942b399102ba0e57
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-12 11:02:49 -0600

    Create db_connection.php

---
+++ db_connection.php
@@ -0,0 +1,21 @@
+<?php
+// Database connection configuration
+$serverName = "tcp:event-mgmt-server.database.windows.net,1433";
+$connectionOptions = array(
+    "UID" => "haya",
+    "pwd" => "Database100",
+    "Database" => "eventplan",
+    "LoginTimeout" => 30,
+    "Encrypt" => 1,
+    "TrustServerCertificate" => 0
+);
+
+try {
+    // Create a new PDO instance
+    $conn = new PDO("sqlsrv:server=$serverName;Database=eventplan", $connectionOptions['UID'], $connectionOptions['pwd']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Handle connection errors
+    die("Error connecting to SQL Server: " . $e->getMessage());
+}
+?>
