81fcca15ee8e0418082fdfdefa707bc77da7d859
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-12 11:07:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,31 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.html">Event Planners Inc.</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="planners.php">Planners</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="clients.php">Clients</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="events.php">Events</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
     <header class="bg-dark text-white text-center py-4">
         <h1>Welcome to The Event-uals!</h1>
         <p class="lead">Your trusted partner for unforgettable events.</p>
