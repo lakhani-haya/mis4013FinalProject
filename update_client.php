ef5a3a31742758df3312c192a32c438fdd5a3ef3
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-13 09:57:16 -0600

    Update update_client.php

--- update_client.php
+++ update_client.php
@@ -1,5 +1,5 @@
 <?php
-// Database connection configuration
+
 $serverName = "tcp:event-mgmt-server.database.windows.net,1433";
 $connectionOptions = array(
     "UID" => "haya",
@@ -14,7 +14,7 @@ try {
     $conn = new PDO("sqlsrv:server=$serverName;Database=eventplan", $connectionOptions['UID'], $connectionOptions['pwd']);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Prepare and execute the update query
+   
     $stmt = $conn->prepare("UPDATE dbo.Clients SET FullName = ?, Email = ?, PhoneNumber = ?, Address = ? WHERE ClientID = ?");
     $stmt->execute([$_POST['fullName'], $_POST['email'], $_POST['phoneNumber'], $_POST['address'], $_POST['clientID']]);
 
