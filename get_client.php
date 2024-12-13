5f77be9f8ccf0515eb00ca758d1b64fa3b8cae20
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-13 09:56:05 -0600

    Update get_client.php

--- get_client.php
+++ get_client.php
@@ -1,5 +1,5 @@
 <?php
-// Database connection configuration
+
 $serverName = "tcp:event-mgmt-server.database.windows.net,1433";
 $connectionOptions = array(
     "UID" => "haya",
@@ -14,7 +14,7 @@ try {
     $conn = new PDO("sqlsrv:server=$serverName;Database=eventplan", $connectionOptions['UID'], $connectionOptions['pwd']);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Prepare and execute the select query
+
     $stmt = $conn->prepare("SELECT * FROM dbo.Clients WHERE ClientID = ?");
     $stmt->execute([$_GET['id']]);
     $client = $stmt->fetch(PDO::FETCH_ASSOC);
