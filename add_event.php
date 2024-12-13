68dc572784549addf017c9e58a5ad20665186b54
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-13 09:24:12 -0600

    Create add_event.php

---
+++ add_event.php
@@ -0,0 +1,25 @@
+<?php
+// Database connection configuration
+$serverName = "tcp:event-mgmt-server.database.windows.net,1433";
+$connectionOptions = array(
+    "UID" => "haya",
+    "pwd" => "Database100",
+    "Database" => "eventplan",
+    "LoginTimeout" => 30,
+    "Encrypt" => 1,
+    "TrustServerCertificate" => 0
+);
+
+try {
+    $conn = new PDO("sqlsrv:server=$serverName;Database=eventplan", $connectionOptions['UID'], $connectionOptions['pwd']);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Prepare and execute the insert query
+    $stmt = $conn->prepare("INSERT INTO dbo.Events (EventType, EventDate, ClientID, PlannerID) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$_POST['eventType'], $_POST['eventDate'], $_POST['clientID'], $_POST['plannerID']]);
+
+    echo "Event added successfully!";
+} catch (PDOException $e) {
+    echo "Error: " . htmlspecialchars($e->getMessage());
+}
+?>
