523cd824bdabdebd0664854755a4635b9554c129
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-12-13 09:54:59 -0600

    Update delete_event.php

--- delete_event.php
+++ delete_event.php
@@ -1,5 +1,5 @@
 <?php
-// Database connection configuration
+
 $serverName = "tcp:event-mgmt-server.database.windows.net,1433";
 $connectionOptions = array(
     "UID" => "haya",
@@ -14,7 +14,7 @@ try {
     $conn = new PDO("sqlsrv:server=$serverName;Database=eventplan", $connectionOptions['UID'], $connectionOptions['pwd']);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Prepare and execute the delete query
+
     $stmt = $conn->prepare("DELETE FROM dbo.Events WHERE EventID = ?");
     $stmt->execute([$_GET['id']]);
 
